6dc71f6d2e38df205274ee82319e9e371fc25318
mackemilian <61463216+mackemilian@users.noreply.github.com>
2020-05-19 14:45:55 +0200

    Kommentarer
    Kommentert:
    main.js
    subTask.js

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
+// main: Grunnlaget for videre utbygging 
+
+// Viktigste kode: 
+// Funksjonen som lager arrayet i local storage: function createList(name)
+// Her kan man legge til flere arrays i tasks (LOCAL_STORAGE_LIST_KEY)
+
+// Kontainer 
 const listsContainer = document.querySelector('[data-lists]')
 const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput = document.querySelector('[data-new-list-input]')
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
+// Hoved localStorage arrayet, det blir generert under i koden, og strings blir pushet andre steder i koden
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+// selectedListId endrer seg i forhold til hvilken oppgave som har blitt trykket på i lists
+// Blir brukt til å vite hvilken task man er på i tasks, så man kan gjøre endringer på riktig sted med å sammenligne id
 let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
 
 const middleListTitle = document.querySelector('[middle-list-title]')
 
+// Click event som endrer selected list, og gjør det mulig å få tak i den verdien
+// Todo endre sånn at selected list ikke kan være ander liste elementer en main-task listen
 listsContainer.addEventListener('click', e => {
+    // Sjekker om tagname til det man har trykket på er li
     if(e.target.tagName.toLowerCase() === 'li') {
+        // ender local storage selected list til verdien av targeted list (listId)
         selectedListId = e.target.dataset.listId
+        // Funskjoner for å lagre og rendre
         currentTaskname()
         saveAndRender()
     }
@@ -24,6 +39,7 @@ newListForm.addEventListener('submit', e => {
     const list = createList(listName)
     newListInput.value = null
     lists.push(list)
+    // Funskjoner for å lagre og rendre
     saveAndRender()
 })
 
@@ -37,19 +53,23 @@ function currentTaskname(){
 }
 
 function createList(name) {
-    return { id: Date.now().toString(), name: name, header: [], tasks: [] }
+    // id blir generert med date så de alltid får en unik id
+    return { id: Date.now().toString(), name: name, header: [], members: [], tasks: [] }
 }
 
 function saveAndRender() {
+    //alle funksjoner som genererer og lagrer verdier
     save()
     render()
     subTaskRender()
 }
 
+// Funksjonen save lagrer verdier i localstorage, så man kan bruke denne når man skal legge inn en ny verdi
 function save() {
     localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
     localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
 }
+
 
 function render() {
     clearElement(listsContainer)
@@ -62,12 +82,14 @@ function render() {
     }
 }
 
+// Funksjon som rendrer (genererer en liste for hver task i tasks, og viser listen med navn <li> tasks.name </li>)
 function taskRender() {
     lists.forEach(list => {
         const listElement = document.createElement('li')
         listElement.dataset.listId = list.id
         listElement.classList.add("list-name")
         listElement.innerText = list.name
+        // sjekker etter selected list og gir den en egen verdi 'avtive-list', man bruker active-list til å ender på selected list i css
         if (list.id === selectedListId) {
             listElement.classList.add('active-list')
         }
@@ -75,10 +97,13 @@ function taskRender() {
     })
 }
 
+// Funskjon som fjerner alle firstChild i et html element
+// TODO: lag en ny funksjon som fjerner mer en bare first child
 function clearElement(element) {
     while(element.firstChild) {
         element.removeChild(element.firstChild)
     }
 }
 
+// rendrer tasks når man refresher siden
 render()
--- subTask.js
+++ subTask.js
@@ -1,31 +1,48 @@
+// subTask: All kode for middleContainer
+
+// form og input til sub-task meny
 const newChildForm = document.querySelector('[data-new-sub-form]')
 const newChildInput = document.querySelector('[data-new-sub-input]')
+// UL til sub-task meny, hvor sub-tasks blir lagt inn som li
 const subUl = document.querySelector('[sub-data-lists]')
 
+// TODO: underveis
+// form og input til sub-task header, (navn man setter selv på sub-task)
 const childHeaderForm = document.querySelector('[data-new-sub-header-form]')
 const childHeaderInput = document.querySelector('[data-new-sub-header]')
 
+// Kontainer til alle sub-task html elementer som blir generert
 const subTaskList = document.getElementsByClassName("subTaskContainer")
 
+// Beskrivelse:
+// Denne koden gjør at vi kan finne oppgaven brukeren er på
+// const selectedList = lists.find(list => list.id === selectedListId)
+
+
+// submit event til sub-tasks, så man kan legge til oppgaver til listen
 newChildForm.addEventListener('submit', e => {
     e.preventDefault();
-
+    
     const subListName = newChildInput.value 
 
     /*if(subListName == null || subListName === "") return*/
 
     const selectedList = lists.find(list => list.id === selectedListId)
 
+    // console.log så man ser hva som skjer
     console.log(selectedList.tasks)
 
     selectedList.tasks.push(subListName)
 
     newChildInput.value = null
 
+    // Save er en global funskjon som lagrer data inn i local storage
     save()
+    // subTaskRenderer gjør at listen blir oppdatert med en gang etter man har lagt til en ny oppgave
     subTaskRender()
 })
 
+// rendrer listen, går gjennom hver string i arrayet tasks og gjør dem om til en (li) list element
 function subTaskRender() {
 
     clearElement(subUl)
@@ -39,10 +56,7 @@ function subTaskRender() {
     })
 }
 
-function subTaskHeaderRender() {
-    clearElement
-}
-
+// Submit event for header(overskrift) til sub-task meny
 childHeaderForm.addEventListener('submit', e => {
     e.preventDefault()
 
@@ -57,10 +71,14 @@ childHeaderForm.addEventListener('submit', e => {
     newListInput.value = null
     console.log("headerCommit");
 
+    // Funskjoner for å lagre og rendre
     subTaskHtmlRender()
     save()
 })
 
+// Render til hele sub-task elementet (html) og input av de lagrede verdiene fra: header [], tasks [].
+//TODO: underveis, subTaskList.appendChild(subTaskCreate) må endres på
+//TODO: må lage en ny konteiner i html som er helt tom, eller en ny clearElement funksjon som fjerner alt. 
 function subTaskHtmlRender(){
     const selectedList = lists.find(list => list.id === selectedListId)
     subTaskName = selectedList.header[0]
@@ -86,5 +104,6 @@ function subTaskHtmlRender(){
     subTaskList.appendChild(subTaskCreate)
 }
 
+// Gjør at subTask meny kommer opp når man refresher siden
 subTaskRender()
 
